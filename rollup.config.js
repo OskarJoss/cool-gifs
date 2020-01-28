9abe2c3e6fe85eaa2dd90d0aaca5315485302a64
oskarjoss <oskar.sjolin.joss@gmail.com>
2020-01-28 12:35:53 +0100

    more setup

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,11 @@ import { terser } from "rollup-plugin-terser";
 import postcss from "rollup-plugin-postcss";
 import normalize from "postcss-normalize";
 import autoprefixer from "autoprefixer";
+import cssnano from "cssnano";
+import babel from "rollup-plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import filesize from "rollup-plugin-filesize";
 const isProduction = process.env.NODE_ENV === "production";
 const isDevelopment = isProduction === false;
 
@@ -16,8 +21,12 @@ module.exports = {
     isDevelopment && browsersync({ server: "public" }),
     isProduction && terser(),
     postcss({
-      plugins: [normalize(), autoprefixer()],
+      plugins: [normalize(), autoprefixer(), cssnano()],
       extract: true
-    })
+    }),
+    babel(),
+    resolve(),
+    commonjs(),
+    isProduction && filesize()
   ]
 };
